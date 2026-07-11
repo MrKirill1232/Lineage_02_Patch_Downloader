325ca60c7a130496ef429c9058bf57f1fb2b6ff7
Index <mrkirill1232@gmail.com>
2026-07-11 09:09:06 +0300

    * update jp link;

--- src/org/index/patchdownloader/enums/CDNLink.java
+++ src/org/index/patchdownloader/enums/CDNLink.java
@@ -6,9 +6,10 @@ public enum CDNLink
     NC_SOFT_TAIWAN("http://mmorepo.cdn.plaync.com.tw/TWLin2EP20/%d/Patch/%s", "http://mmorepo.cdn.plaync.com.tw/TWLin2EP20/%d/Patch/PatchFileInfo_TWLin2EP20_%d.dat"),
     NC_SOFT_KOREAN("http://l2kor.ncupdate.com/%s", "http://l2kor.ncupdate.com/L2_KOR/%d/Patch/files_info.json.zip"),
     // http://l2-client-cdn.ncsoft.jp/L2_JP/102/Patch/Zip/Maps/24_14.unr.zip
+    // http://ncj-L2-asset.ncsoft.jp/L2_JP/212/Patch/Zip/Maps/24_14.unr.zip
     // 101 - 464
     // 102 - 474
-    NC_SOFT_JAPANESE("http://l2-client-cdn.ncsoft.jp/L2_JP/%d/Patch/%s", "http://l2-client-cdn.ncsoft.jp/L2_JP/%d/Patch/PatchFileInfo_L2_JP_%d.dat"),
+    NC_SOFT_JAPANESE("http://ncj-L2-asset.ncsoft.jp/L2_JP/%d/Patch/%s", "http://ncj-L2-asset.ncsoft.jp/L2_JP/%d/Patch/PatchFileInfo_L2_JP_%d.dat"),
     // NC-WEST IS DEAD :D
     // http://d35293xeakkyq4.cloudfront.net/LINEAGE2/479/Patch/FileInfoMap_LINEAGE2_479.dat.zip
     // [Request URI: http://d35293xeakkyq4.cloudfront.net/LINEAGE2/479/Patch/PatchFileInfo_LINEAGE2_479.dat.zip]
